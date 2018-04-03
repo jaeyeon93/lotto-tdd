4fef80adc374a3c78518d4f5ad74df70fb9baa31
jimmy <foodsksms@gmail.com>
2018-04-03 16:14:14 +0900

    Lotto project start

---
+++ src/main/java/lotto/Lotto.java
@@ -0,0 +1,43 @@
+package lotto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Lotto {
+    private List<Integer> lotto;
+    static List<Integer> numbers;
+    static {
+        numbers = init();
+    }
+    public Lotto() {
+        Collections.shuffle(numbers);
+        lotto = new ArrayList<>();
+        for(int i = 0; i < 6; i++)
+            lotto.add(numbers.get(i));
+    }
+
+    public Lotto(String number) {
+        lotto = new ArrayList<>();
+        List<String> stringNum = Arrays.asList(number.split(","));
+        for (String s: stringNum) {
+            lotto.add(Integer.parseInt(s));
+        }
+    }
+
+    public static List<Integer> init() {
+        List<Integer> values = new ArrayList<>();
+        for(int i = 1; i < 46; i++)
+            values.add(i);
+        return values;
+    }
+
+    public Integer lottoSize() {
+        return lotto.size();
+    }
+
+    public List<Integer> getLotto() {
+        return lotto;
+    }
+}
